d2b489bba763514f0e423e0c33d20c1e34441586
Mary Brown <marycbrown2004@gmail.com>
2020-07-14 17:17:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a = "Mary"
+b = "Mandopony"
+c = "Canada"
+d = "Drawing"
+e = "July,14th,2020"
+
+print ("My name: " + a)
+print ("My favorite musician: " + b)
+print ("A place I'd like to visit: " + c)
+print ("A hobby of mine: " + d)
+print ("Todays date: " + e)
